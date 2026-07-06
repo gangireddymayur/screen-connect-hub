fd4e7bfe96c3669375b8079d96b8dba4803c57bf
Mayur <mayurgangereddy12345@gmail.com>
2026-07-06 11:12:57 +0530

    refactor: trigger auto build bot

--- src/pages/admin/AdminSchedulePage.tsx
+++ src/pages/admin/AdminSchedulePage.tsx
@@ -1,3 +1,4 @@
+// Screen scheduling page with double-click selection and range actions
 import * as React from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
